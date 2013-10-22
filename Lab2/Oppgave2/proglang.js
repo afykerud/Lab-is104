9115d030d12471584bae83300ceb3a5f7879a19c
afykerud <andersfykerud@hotmail.com>
2013-10-22 17:36:13 +0200

    dette er da alle lab filer
    axzcz<xz<

---
+++ Lab2/Oppgave2/proglang.js
@@ -0,0 +1,12 @@
+function printToOutputBox(selectobj) {
+var e_proglangs = document.getElementById("proglangs");
+var e_outputbox = document.getElementById("outputbox");
+e_outputbox.innerHTML = "Du har valgt programmeringsspråket " + e_proglangs.	// Forandrer innholdet i outputbox
+options[selectobj.selectedIndex].text;
+
+}
+
+function myFunction()
+{
+alert("Beklager så mye, denne siden er under konstruksjon!");	// Gjør at funksjonen sender et varsel.
+}
---
+++ Lab2/Readme.txt
@@ -0,0 +1,10 @@
+Kontrollspørsmål lab 2
+Modell:
+Vil si dette er er teksten, her på min side informasjonen om programmeringsspråk.
+
+Presentasjon:
+vil si dette er menyen til venstre og skriften på i senter. altså bruker grensesnittet
+
+Kontroller:
+dette er når brukeren trykker på en linkene, når det da starter en JavaScript funksjon. på min side når det egentlig skulle kommer et varsel der det står at siden
+er under konstruksjon.
